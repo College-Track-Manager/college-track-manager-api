a4381d936158920b45242972c668a7333912c389
YaserElbidwehy <yaser_dba@hotmail.com>
2025-04-15 14:40:12 +0300

    add Study Type

---
+++ Migrations/20250415113205_AddStudyTypeToStudentRegistration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CollegeTrackAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStudyTypeToStudentRegistration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "StudyType",
+                table: "Registrations",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StudyType",
+                table: "Registrations");
+        }
+    }
+}
